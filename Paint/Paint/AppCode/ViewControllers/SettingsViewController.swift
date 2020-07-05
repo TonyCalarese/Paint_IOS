fad402a81b2da80a2a2bb07a90f38b188c7ffc7c
Tony Calarese <accalarese@gmail.com>
2020-07-05 16:18:40 -0400

    Finished App, Need to adjust UI
    Forgot to commit my final app and going to be working on the UI for multiple screen sizes

--- Paint/Paint/AppCode/ViewControllers/SettingsViewController.swift
+++ Paint/Paint/AppCode/ViewControllers/SettingsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    //Outlets
     @IBOutlet var widthLabel: UILabel!
     @IBOutlet var widthValue: UILabel!
     @IBOutlet var opacityLabel: UILabel!
@@ -18,7 +18,28 @@ class SettingsViewController: UIViewController {
     @IBOutlet var widthSlider: UISlider!
     @IBOutlet var opacitySlider: UISlider!
     
-    @IBOutlet var blackColorButton: UIButton!
+    //Actions
+    //Brush Colors
+    @IBAction func blackColorButton(_ sender: Any) {
+        setBrushColor(UIColor.black)
+    }
+    @IBAction func redColorButton(_ sender: Any) {
+        setBrushColor(UIColor.red)
+    }
+    
+    @IBAction func blueColorButton(_ sender: Any) {
+        setBrushColor(UIColor.blue)
+    }
+    @IBAction func orangeColorButton(_ sender: Any) {
+        setBrushColor(UIColor.orange)
+    }
+    @IBAction func pinkColorButton(_ sender: Any) {
+        setBrushColor(UIColor.systemPink)
+    }
+    @IBAction func greenColorButton(_ sender: Any) {
+        setBrushColor(UIColor.green)
+    }
+    
     
     //Adjusting the sliders
     @IBAction func adjWidth(_ sender: UISlider) {
@@ -27,9 +48,35 @@ class SettingsViewController: UIViewController {
     @IBAction func adjOpactity(_ sender: UISlider) {
         setLabelText(label: opacityValue, value: String(sender.value))
     }
+    //Dynamic function for setting label text
+     func setLabelText(label: UILabel, value: String) {
+         label.text = value
+     }
+     
+     func setBrushColor(_ color: UIColor){
+           UserDefaults.standard.setColor(color: color, forKey: "color") //set Color
+     }
+     
+     func getWidth_Opacity() -> (CGFloat, CGFloat){
+         var w: CGFloat = 10.0, o: CGFloat = 10.0
+         if let width = UserDefaults.standard.string(forKey: "width") {
+             w = CGFloat(truncating: NumberFormatter().number(from: width)!)
+                    
+                }
+         if let opacity = UserDefaults.standard.string(forKey: "opacity") {
+             o = CGFloat(truncating: NumberFormatter().number(from: opacity)!)
+
+                }
+         return (w, o)
+     }
+
     //On load the labels need to adjust to the value of the slider
     override func viewDidLoad() {
         super.viewDidLoad()
+        let (width, opacity) = getWidth_Opacity()
+        widthSlider.setValue(Float(width), animated: true)
+        opacitySlider.setValue(Float(opacity), animated: true)
+        
         // Do any additional setup after loading the view.
        setLabelText(label: widthValue, value: String(widthSlider.value))
         setLabelText(label: opacityValue, value: String(opacitySlider.value))
@@ -38,16 +85,13 @@ class SettingsViewController: UIViewController {
     //When the user leaves set the user defaults and settings over
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        UserDefaults.standard.setColor(color: UIColor.red, forKey: "color") //set Color
+        //UserDefaults.standard.setColor(color: UIColor.red, forKey: "color") //set Color
+        //It is more costly to set the width and opacity everytime it is changed so going to change it on destroy
         UserDefaults.standard.set(widthSlider.value, forKey: "width") //Set Width
         UserDefaults.standard.set(opacitySlider.value, forKey: "opacity") //set Opacity
         
         paintbrush?.buildBrush() //rebuild the brush
     }
  
-    //Dynamic function for setting label text
-    func setLabelText(label: UILabel, value: String) {
-        label.text = value
-    }
-
+ 
 }
